2cdcead3bccbf3ca07355abd050398fbf9692bd2
Ben Sless <ben.sless@gmail.com>
2021-05-25 14:55:26 +0300

    WIP

--- src/com/github/bsless/malli_keys_relations/constraints.clj
+++ src/com/github/bsless/malli_keys_relations/constraints.clj
@@ -1,5 +1,70 @@
 (ns com.github.bsless.malli-keys-relations.constraints)
 
+(defprotocol Views
+  (-view? [this]))
+
+(defprotocol View
+  (-view [this that])
+  (-children-views [this])
+  (-form [this]))
+
+(let [extend (fn [_protocol this]
+               (let [v? (satisfies? View this)]
+                 (extend-protocol Views (class this)
+                                  (-view? [_] v?))))]
+  (extend-protocol Views
+    nil
+    (-view? [_] false)
+    Object,
+    (-view? [this] (extend View this) (satisfies? View this))))
+
+(defn -create-form [type children]
+  (into [type] children))
+
+(defmethod print-method ::view
+  [v ^java.io.Writer w]
+  (.write w (pr-str (-form ^View v))))
+
+(defn -const-view
+  [x]
+  (let [form (-create-form :view/const [x])]
+    ^{:type ::view}
+    (reify View
+      (-view [_ _] x)
+      (-children-views [this])
+      (-form [_] form))))
+
+(comment
+  (def cv (-const-view 1))
+  (-view cv 'foo)
+  (-children-views cv)
+  (-form cv))
+
+(defn- -path-getter
+  [path]
+  (reduce
+   (fn [f e]
+     (fn [m]
+       (get (f m) e)))
+   identity
+   path))
+
+(defn -path-view
+  [path]
+  (let [form (-create-form :view/path path)
+        getter (-path-getter path)]
+    ^{:type ::view}
+    (reify View
+      (-view [_ m] (getter m))
+      (-children-views [this] path)
+      (-form [_] form))))
+
+(comment
+  (def pv (-path-view [:a :b]))
+  (-view pv {:a {:b 1}})
+  (-children-views pv)
+  (-form pv))
+
 ;; (def -value-where nil)
 
 (defmulti -value-where (fn [args] (if (vector? args) (nth args 0) :view/const)))
